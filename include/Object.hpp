01a876b788301f21daf70205bc4a56460f320fc2
João Gerliandro <joaogerliandro@gmail.com>
2022-11-11 18:03:40 -0300

    Initial Object Class structure
    I Will update this in the Future

--- include/Object.hpp
+++ include/Object.hpp
@@ -1,1 +1,27 @@
 #pragma once
+
+#include <vector>
+#include <string>
+#include <sstream>
+#include <ifstream>
+
+#include <glm/glm.hpp>
+#include <glm/ext.hlp>
+
+using namespace DeepDarkStreets
+{
+   public class Object
+   {
+        public:
+            Object();
+
+            Objetct(std::stringview path_stream);
+        private:
+            std::vector<glm::fvec3> vertex_list;
+            std::vector<glm::fvec2> texture_cords;
+            std::vector<glm::fvec2> normals_list;
+
+            std::vector<glm::fvec3> positivo;
+    };
+}
+
